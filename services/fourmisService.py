4c804521a83028ce60256152ea00fa5d388dcc5e
Antonin <85553842+AntoninMart@users.noreply.github.com>
2023-06-19 17:35:17 +0200

    add algo fourmis service

---
+++ services/fourmisService.py
@@ -0,0 +1,10 @@
+class FourmisService:
+    def __init__(self) -> None:
+        pass
+
+    def fourmitalisation(self, allCity, map):
+        pass
+
+class ant:
+    def __init__(self) -> None:
+        pass
