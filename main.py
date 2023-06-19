820a80f00183f6cd4e527e6fb50b0c46c3bc540b
Antonin <85553842+AntoninMart@users.noreply.github.com>
2023-06-19 14:12:00 +0200

    add completeGraph

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from services.cityService import CityService
+from services.mapsService import Maps
 import pandas
+import networkx as nx
 import folium
 
 if __name__ == "__main__":
     city_service = CityService()
-    allCity = city_service.load_cities("C:\\Users\\32pyr\\OneDrive\\Bureau\\Projet\\Projet\\data\\villes10.txt")
+    map_service = Maps()
+    allCity = city_service.load_cities(".\\data\\villes1000.txt")
     
     for city in allCity:
         print(city.Name, city.X, city.Y)
@@ -19,6 +22,7 @@ if __name__ == "__main__":
     dataFrame = pandas.DataFrame(data, columns=['Ville', 'Latitude', 'Longitude'])
     map = folium.Map(location=[46.603354, 1.888334], zoom_start=6)  # Coordonnées du centre de la France
     for index, row in dataFrame.iterrows():
-        folium.Marker([row['Latitude'], row['Longitude']], popup=row['Ville']).add_to(map)
+        folium.Marker([row['Latitude'], row['Longitude']], popup=row['Ville'], ).add_to(map)
+
     
-    map.save('C:\\Users\\32pyr\\OneDrive\\Bureau\\Projet\\Projet\\france_cities_map10.html')  # Sauvegarde la carte dans un fichier HTML
+    map.save('france_cities_map1000.html')  # Sauvegarde la carte dans un fichier HTML
