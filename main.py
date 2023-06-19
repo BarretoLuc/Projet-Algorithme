a389a770b1a25ddfe13b96266b17631b0cf76dc6
BarretoLuc <barreto.luc.02@gmail.com>
2023-06-19 15:30:23 +0200

    Intégration de l'affichage de la map.

--- main.py
+++ main.py
@@ -2,59 +2,51 @@ from services.cityService import CityService
 from services.dijkstraService import DijkstraService
 from services.mapsService import MapsService
 from geopy.distance import geodesic
-import pandas, folium, sys, random
+import sys, random
 
 if __name__ == "__main__":
-    city_service = CityService()
-    map_service = MapsService()
-    allCity = city_service.load_cities(".\\data\\villes10.txt")
+    
+    # Récupération des coordonnées des villes :
+    cityService = CityService()
+    listCoordCity = cityService.load_cities(".\\data\\villes10.txt")
     
     print("Liste des villes :")
-    for city in allCity:
+    for city in listCoordCity:
         print(city.Name, city.X, city.Y)
     
-    #Convertion en DataFrame pour l'affichage sur la carte
-    data = {
-        "Ville": [o.Name for o in allCity],
-        "Latitude": [o.X for o in allCity],
-        "Longitude": [o.Y for o in allCity]
-    }
-    
-    dataFrame = pandas.DataFrame(data, columns=['Ville', 'Latitude', 'Longitude'])
-    map = folium.Map(location=[46.603354, 1.888334], zoom_start=6)  # Coordonnées du centre de la France
-    for index, row in dataFrame.iterrows():
-        folium.Marker([row['Latitude'], row['Longitude']], popup=row['Ville'], ).add_to(map)
+    # Calcul de la matrice des distances entre les villes :
+    n = len(listCoordCity)
+    matriceDistanceCity = [[0] * n for _ in range(n)] # Initialisez une matrice de distances remplie de zéros.
 
-    
-    map.save('.\\maps\\france_cities_map10.html')  # Sauvegarde la carte dans un fichier HTML
-    
-    print("\nLa map a été générée avec succès !\n")
-        
-    n = len(allCity)
-
-    # Initialisez une matrice de distances remplie de zéros
-    distances = [[0] * n for _ in range(n)]
-
-    # Calculez les distances entre les villes
-    for i in range(n):
+    for i in range(n): # Calcul des distances entre les villes et ajout dans la matrice.
         for j in range(i+1, n):    
-            coord1 = (allCity[i].X, allCity[i].Y)
-            coord2 = (allCity[j].X, allCity[j].Y)
+            coord1 = (listCoordCity[i].X, listCoordCity[i].Y)
+            coord2 = (listCoordCity[j].X, listCoordCity[j].Y)
             distance = geodesic(coord1, coord2).kilometers
-            if(distance > 200 and random.randint(0, 1) == 0):
+            if(distance > 200 and random.randint(0, 1) == 0): # On supprime certaines distances pour avoir un graphe non connexe.
                 distance = 0
-            distances[i][j] = distance
-            distances[j][i] = distance
+            matriceDistanceCity[i][j] = distance
+            matriceDistanceCity[j][i] = distance
 
-    # Affichez la matrice de distances
     print("Matrice de distances :\n")
-    for row in distances:
+    for row in matriceDistanceCity:
         print(row)
-
+        
+    # Création de la carte des villes avec les distances entre elles :    
+    mapService = MapsService(listCoordCity)
+    mapService.matriceGraph(listCoordCity, matriceDistanceCity)
+    mapService.saveMap('.\\maps\\france_cities_map10.html')
+    print("\nLa map a été générée avec succès !\n")
+    
+    
     print("\nDijkstra :\n")
     dijkstra_service = DijkstraService()
-    dijkstra = dijkstra_service.find_all(distances, 0, 6)
     
+    dijkstra = dijkstra_service.find_all(matriceDistanceCity, 0, 6)
+    # 0 / 4 / 6
+    
+    # 20 / 0 4
+    # 40 / 0 3 5 6
     print(dijkstra)
     
 sys.exit(0)
